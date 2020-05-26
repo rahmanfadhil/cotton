ddc06038be3e14b8254e217dd261f1f1afc3afa5
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-05-26 22:14:24 +0700

    Fix env variable name in adapter/sqlite_test.ts

--- src/adapter/sqlite_test.ts
+++ src/adapter/sqlite_test.ts
@@ -2,7 +2,7 @@ import { SqliteAdapter } from "./sqlite.ts";
 
 Deno.test("SqliteAdapter: should connect to database and disconnect to database", async () => {
   const adapter = new SqliteAdapter({
-    database: Deno.env.get("TEST_SQLITE_DATABASE"),
+    database: Deno.env.get("SQLITE_DATABASE"),
   });
   await adapter.connect();
   await adapter.disconnect();
