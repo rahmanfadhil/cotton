b4e8fc282261314d3771187f6a3db22d1a51a005
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-05-30 07:31:08 +0700

    Add MySQL connection test case in connect_test.ts

--- src/connect_test.ts
+++ src/connect_test.ts
@@ -3,12 +3,13 @@ import { assertEquals } from "../testdeps.ts";
 import { connect } from "./connect.ts";
 import { SqliteAdapter } from "./adapter/sqlite.ts";
 import { PostgresAdapter } from "./adapter/postgres.ts";
-// import { MysqlAdapter } from "./adapter/mysql.ts";
+import { MysqlAdapter } from "./adapter/mysql.ts";
+import { mysqlOptions, postgresOptions, sqliteOptions } from "./testutils.ts";
 
 Deno.test("connect: sqlite", async () => {
   const db = await connect({
     type: "sqlite",
-    database: Deno.env.get("SQLITE_DATABASE"),
+    ...sqliteOptions,
   });
   assertEquals(db instanceof SqliteAdapter, true);
   await db.disconnect();
@@ -17,24 +18,14 @@ Deno.test("connect: sqlite", async () => {
 Deno.test("connect: postgres", async () => {
   const db = await connect({
     type: "postgres",
-    database: Deno.env.get("POSTGRES_DATABASE"),
-    hostname: Deno.env.get("POSTGRES_HOSTNAME"),
-    username: Deno.env.get("POSTGRES_USERNAME"),
-    password: Deno.env.get("POSTGRES_PASSWORD"),
-    port: Number(Deno.env.get("POSTGRES_PORT")) || 5432,
+    ...postgresOptions,
   });
   assertEquals(db instanceof PostgresAdapter, true);
   await db.disconnect();
 });
 
-// Deno.test("connect: mysql", async () => {
-//   const db = await connect({
-//     type: "mysql",
-//     database: env.TEST_MYSQL_DATABASE,
-//     hostname: env.TEST_MYSQL_HOSTNAME,
-//     username: env.TEST_MYSQL_USERNAME,
-//     password: env.TEST_MYSQL_PASSWORD,
-//   });
-//   assertEquals(db instanceof MysqlAdapter, true);
-//   await db.disconnect();
-// });
+Deno.test("connect: mysql", async () => {
+  const db = await connect({ type: "mysql", ...mysqlOptions });
+  assertEquals(db instanceof MysqlAdapter, true);
+  await db.disconnect();
+});
