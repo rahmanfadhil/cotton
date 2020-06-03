c61345cb4de1aa3098d677cd8aa1cc9705d8627d
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-06-03 21:16:32 +0700

    Update mysql driver to v2.2.0

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export {
   Client as MysqlClient,
   ClientConfig as MysqlClientConfig,
-} from "https://deno.land/x/mysql@2.1.0/mod.ts";
+} from "https://deno.land/x/mysql@2.2.0/mod.ts";
 export { Client as PostgresClient } from "https://deno.land/x/postgres@v0.4.1/mod.ts";
 export {
   DB as SqliteDB,
