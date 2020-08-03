311a29cca36d9ef5982c61f847cd46d5fc9d3550
Rahman Fadhil <rhmnfadhil@gmail.com>
2020-08-04 00:20:20 +0700

    Add TSDoc to serializer utilities

--- src/utils/serializers.ts
+++ src/utils/serializers.ts
@@ -2,6 +2,11 @@ import { SerializableDescription } from "../serializers/decorators/serializer.ts
 import { Reflect } from "./reflect.ts";
 import { metadata } from "../constants.ts";
 
+/**
+ * Retrieve all serializable properties in a model class.
+ * 
+ * @param target the model class you want to retrieve the information from
+ */
 export function getProperties(target: Function): SerializableDescription[] {
   const properties = Reflect.getMetadata(
     metadata.serializables,
